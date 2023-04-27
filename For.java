6eccc399f1f63b38cbf78d913fdcc2b6911bcd84
clonoJerry <ziyu3188@naver.com>
2023-04-27 18:18:52 +0900

    first

---
+++ For.java
@@ -0,0 +1,33 @@
+public class For{
+
+	public static void main(String [] args){
+
+		for(int a = 1; a <= 5; a++){
+			for(int b = 5; b >= a; b--){
+
+				System.out.print(" ");
+			} 
+			
+			for(int c = 1; c <= a; c++){
+				
+
+				System.out.print("*");
+				
+
+			}
+			for(int d = 2; d <= a; d++){
+				
+
+				System.out.print("*");
+				
+
+			}
+			
+
+
+			System.out.println("");
+		}	
+
+	}
+
+}
